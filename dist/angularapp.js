fd6dc4ff3cae6db5c10582b55ca4aae70d763f58
urrik98 <adamrenschen@gmail.com>
2016-07-29 15:10:49 -0700

    Added better text to form fields

--- dist/angularapp.js
+++ dist/angularapp.js
@@ -1,1 +1,41 @@
-angular.module("client",[]).controller("DisplayController",function(n,o){angular.extend(n,o)}).controller("FormController",function(n,o){angular.extend(n,o)}).factory("getActivities",function(n){var o=[],t=function(n){return n="("+n.slice(0,3)+") "+n.slice(3,6)+"-"+n.slice(6,10)},e=function(e){console.log("data to server",e),o.length=0,e.forEach(function(e){e&&n.post("/callYelp",[e]).then(function(n){n.data.phone=t(n.data.phone),o.push(n.data),console.log(o)})})};return{postToServer:e,results:o,formatPhone:t}});
+
+angular.module('client',[])
+
+.controller('DisplayController', function($scope, getActivities) {
+  angular.extend($scope, getActivities);
+})
+
+.controller('FormController', function($scope, getActivities) {
+  angular.extend($scope, getActivities);
+})
+
+.factory('getActivities', function($http) {
+  var results = [];
+
+  var formatPhone = function(phone) {
+      return phone = "("+ phone.slice(0,3) + ") " + phone.slice(3, 6) + "-" + phone.slice(6, 10);
+  };
+
+  var postToServer = function(data) {
+    console.log("data to server", data);
+    results.length = 0;
+    data.forEach(function(term){
+      if (term) {
+        $http.post('/callYelp', [term])
+        .then(function(response) {
+          response.data.phone = formatPhone(response.data.phone);
+          results.push(response.data);
+          console.log(results);
+        })
+      }
+    });
+  };
+
+  return {
+    postToServer: postToServer,
+    results: results,
+    formatPhone: formatPhone
+  };
+
+
+})
